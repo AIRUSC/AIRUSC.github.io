d8da231f481bd794a59a216fb62b14935186cf66
AIRUSC <3458038461@qq.com>
2020-01-19 22:41:17 +0800

    Site updated: 2020-01-19 22:41:05

---
+++ dist/music.js
@@ -0,0 +1,13 @@
+const ap = new APlayer({
+    container: document.getElementById('aplayer'),
+    fixed: true,
+    autoplay: true,
+    audio: [
+      {
+        name: "年少有为",
+        artist: '李荣浩',
+        url: 'http://www.170mv.com/kw/antiserver.kuwo.cn/anti.s?rid=MUSIC_47502984&response=res&format=mp3|aac&type=convert_url&br=128kmp3&agent=iPhone&callback=getlink&jpcallback=getlink.mp3',
+        cover: 'https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1579452391348&di=9d3fe4eb362a07f785c2668b8c504d46&imgtype=0&src=http%3A%2F%2Fp1.music.126.net%2F8nnGrPjpr_oCxTq6_Ac5qQ%3D%3D%2F109951163636915574.jpg',
+      },
+    ]
+});
